9e0817fbc01bbd1867e1e1c6d73b56af6676ff18
HappySamson <149072583+HappySamson@users.noreply.github.com>
2024-09-12 10:18:29 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#222222"
+        "value": "#f2f2f2"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#333333",
+        "color": "#660033",
         "opacity": 0.4,
         "width": 1
       },
